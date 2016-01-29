0b74d516531393f705ad9450d4a65cd4292b606e
John Janzen <joalja2002@msn.com>
2016-01-29 14:58:34 -0800

    LockEmUp LINK

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -33,6 +33,16 @@ class Welcome extends Application {
 
 		$this->render();
 	}
+        
+        function shucks() 
+        {
+            $record = $this->quotes->get(2);
+            $this->data = array_merge($this->data, $record);
+                
+            $this->data['pagebody'] = 'justone';
+                
+            $this->render();
+        }
 
 }
 
